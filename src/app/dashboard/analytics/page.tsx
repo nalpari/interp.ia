06d91cc351514191b5d8788d0eedf5dd5b6f3c1e
yoosangwook <yoo32767@gmail.com>
2025-03-11 14:16:01 +0900

    fix: page 수정

--- src/app/dashboard/analytics/page.tsx
+++ src/app/dashboard/analytics/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function page() {
-  return <h1>Analytics</h1>;
+  return (
+    <div className="container mx-auto py-10">
+      <h1 className="text-3xl font-bold mb-6">Analitics</h1>
+    </div>
+  );
 }
